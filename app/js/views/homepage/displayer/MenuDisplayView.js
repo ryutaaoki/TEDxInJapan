a9f4f1ee8da23ff760dd41114935c1ab05ef5806
Stéphane Vieira <vieira.stephane@gmail.com>
2013-04-24 22:37:08 +0200

    detect change displayer view on the homepage

--- app/js/views/homepage/displayer/MenuDisplayView.js
+++ app/js/views/homepage/displayer/MenuDisplayView.js
@@ -21,6 +21,12 @@ define([
   var logger = woodman.getLogger('views.MenuDisplayView');
   var MenuDisplayView = Item.extend({
 
+    events: {
+      'click #tedx-now'  : 'tedxLive',
+      'click #tedx-map'  : 'tedxMap',
+      'click #tedx-radio': 'tedxRadio'
+    },
+
     initialize: function(options) {
       logger.info('initialize MenuDisplayView');
       var options = options || {};
@@ -29,6 +35,27 @@ define([
 
       Item.prototype.initialize.call(this,options);
     },
+
+    tedxLive: function () {
+      logger.info('Gotcha ! TEDx Live !');
+      // this.$el.find($('img')).first().show();
+    },
+
+    tedxMap: function () {
+      logger.info('Display Google Maps API with TEDx in the country !');
+      // this.$el.find($('img')).first().hide();
+
+
+      // var gmap = new GMap();
+      // console.log(gmap);
+      /* render the map GoogleMaps API v3 */
+
+    },
+
+    tedxRadio: function () {
+      logger.info('Edit view Radio to listen TEDx live radio !');
+      // this.$el.find($('img')).first().show();
+    }
   });
 
   return MenuDisplayView;
