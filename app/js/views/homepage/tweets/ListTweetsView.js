9e1deac5e0f247fa70e75b7bc05bbf3705897dc1
Stéphane Vieira <vieira.stephane@gmail.com>
2013-04-25 16:52:27 +0200

    Display tweets datasource in homepage

--- app/js/views/homepage/tweets/ListTweetsView.js
+++ app/js/views/homepage/tweets/ListTweetsView.js
@@ -24,8 +24,30 @@ define([
 
     initialize: function(options) {
       logger.info('initialize ListTweets');
+      // options.template = ItemTweetsTemplate;
+
+      var tweets = Joshfire.factory.getDataSource('tweets');
+
+      tweets.find({
+          limit: 10
+        },
+        function( err , data ) {
+          var html = "<ul>";
+          _.each(data.entries, function(entry) {
+            console.log(entry);
+            html += '<a href="' + entry.url + '" target="_blank">';
+            html += '<li class="tweet">';
+            html += '<img src="' + entry.author[0].image.contentURL + '" alt="" width="40"/>';
+            html += '<span>' + entry.name + '</span>' + '<br/><span class="name-author">' + entry.author[0].name + '</span>';
+            html += '</li></a>';
+          });
+          html +="</ul>";
+          $("#posts").html(html);
+        }
+      );
+
       var options = options || {};
-      options.template = ItemTweetsTemplate;
+
 
       List.prototype.initialize.call(this,options);
     },
